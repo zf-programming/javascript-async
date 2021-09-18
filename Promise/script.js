e956a0564be74f23f57d1a5d908384690ec50fb4
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-18 08:22:37 +0700

    Promise

---
+++ Promise/script.js
@@ -0,0 +1,75 @@
+// seleksi input dan button
+const input = document.querySelector('#search-movie');
+const button = document.querySelector('#search');
+
+// event button
+button.addEventListener('click', (e) => {
+  // panggil getMovies
+  const promise = getMovies();
+  console.log(promise);
+
+  // Clicked the button
+  console.log('Okay');
+
+  // clear input
+  clearInput();
+});
+
+// getMovies dengan AJAX + Promise
+function getMovies() {
+  // cari movies berdasarkan keyword
+  const url = getMoviesURL(input.value);
+
+  // Promise
+  const promise = new Promise((resolve, reject) => {
+    // AJAX
+    const ajax = new XMLHttpRequest(); // membuat ajax
+    ajax.open('GET', url); // set request ke url
+    ajax.send(); // kirimkan request
+    // AJAX Callback (akan di eksekusi setelah proses ajax selesai)
+    ajax.onload = () => {
+      console.log(ajax.status);
+      if (ajax.status == 200) {
+        const response = JSON.parse(ajax.responseText);
+        const data = response.Search;
+        resolve(data);
+      } else {
+        reject(Error('Gagal mengambil data'));
+      }
+    };
+  });
+
+  return promise;
+}
+
+// function jika ajax error
+function getMoviesError() {
+  console.error('Error get Movies');
+  alert('Error get Movies');
+}
+
+// clear input
+function clearInput() {
+  input.value = '';
+}
+
+// function mencari movies dari url
+function getMoviesURL(keyword) {
+  return `http://www.omdbapi.com/?apikey=c8800146&s=${keyword}`;
+}
+
+// function memasukkan list ke ul
+function displayMovieList(data) {
+  const movieContainer = document.querySelector('.movies');
+  const movies = createMovieList(data);
+  movieContainer.innerHTML = movies;
+}
+
+// function membuat list untuk movie
+function createMovieList(movies) {
+  let list = '';
+  movies.forEach((movie) => {
+    list += `<li>${movie.Title}</li>`;
+  });
+  return list;
+}

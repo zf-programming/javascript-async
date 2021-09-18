78dd0d04d1fa7b49f55c6493b0eed71efec77d70
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-18 09:21:46 +0700

    Promise All Method

--- Promise/script.js
+++ Promise/script.js
@@ -1,33 +1,46 @@
 // seleksi input dan button
 const input = document.querySelector('#search-movie');
+const input2 = document.querySelector('#search-movie2');
+const input3 = document.querySelector('#search-movie3');
 const button = document.querySelector('#search');
 
 // event button
 button.addEventListener('click', (e) => {
   // panggil getMovies
-  const promise = getMovies();
-  promise
-    .then((value) => value.Search) // method jika promise resolved/fullfiled
-    .then((data) => displayMovieList(data))
-    .catch((error) => alert(error)) // method jika promise rejected
-    .finally(() => console.log('Promise Selesai')); // method yang akan di eksekusi di akhir promise
-  // Clicked the button
-  console.log('Okay');
+  // const promise = getMovies(input.value);
+  // promise
+  //   .then((value) => value.Search) // method jika promise resolved/fullfiled
+  //   .then((data) => displayMovieList(data))
+  //   .catch((error) => alert(error)) // method jika promise rejected
+  //   .finally(() => console.log('Promise Selesai')); // method yang akan di eksekusi di akhir promise
+  // // Clicked the button
+  // console.log('Okay');
 
+  // Promise All
+  const promise = getMovies(input.value);
+  const promise2 = getMovies(input2.value);
+  const promise3 = getMovies(input3.value);
+
+  Promise.all([promise, promise2, promise3])
+    .then((values) => values.map((value) => value.Search))
+    .then((data) =>
+      data.forEach((videos) => {
+        displayMovieList(videos);
+      })
+    );
+  // clear ul
+  clearMovieList();
   // clear input
   clearInput();
 });
 
 // getMovies dengan AJAX + Promise
-function getMovies() {
-  // cari movies berdasarkan keyword
-  const url = getMoviesURL(input.value);
-
+function getMovies(keyword) {
   // Promise
   const promise = new Promise((resolve, reject) => {
     // AJAX
     const ajax = new XMLHttpRequest(); // membuat ajax
-    ajax.open('GET', url); // set request ke url
+    ajax.open('GET', getMoviesURL(keyword)); // set request ke url
     ajax.send(); // kirimkan request
     // AJAX Callback (akan di eksekusi setelah proses ajax selesai)
     ajax.onload = () => {
@@ -53,6 +66,8 @@ function getMoviesError() {
 // clear input
 function clearInput() {
   input.value = '';
+  input2.value = '';
+  input3.value = '';
 }
 
 // function mencari movies dari url
@@ -64,7 +79,7 @@ function getMoviesURL(keyword) {
 function displayMovieList(data) {
   const movieContainer = document.querySelector('.movies');
   const movies = createMovieList(data);
-  movieContainer.innerHTML = movies;
+  movieContainer.innerHTML += movies;
 }
 
 // function membuat list untuk movie
@@ -74,4 +89,9 @@ function createMovieList(movies) {
     list += `<li>${movie.Title}</li>`;
   });
   return list;
+}
+
+// function clear Movie List (ul)
+function clearMovieList() {
+  document.querySelector('.movies').innerHTML = '';
 }
